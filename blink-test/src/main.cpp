9812b57c9fb7fbf3d1e564b6f5936c320cd875ba
Edward <securedbread@protonmail.com>
2024-03-10 01:09:39 -0800

    Added the blink test to prove a ground zero for uploading code to the esp32. I want to be sure that the code works and the ESP isnt also the problem, unless there is a semantic error.

---
+++ blink-test/src/main.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+
+#define led 2
+
+void setup() {
+  pinMode(led, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(led, HIGH);
+  delay(500);
+  digitalWrite(led, LOW);
+  delay(500);
+  // digitalWrite(led, HIGH);
+}
